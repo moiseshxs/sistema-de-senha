e2c02e28852c538ed5efe1d252a9729603289a17
Miguel <migramoslim@gmail.com>
2024-05-12 22:35:47 -0300

    reformular arquivo triagem.js

---
+++ app/Controller/recallSenha.php
@@ -0,0 +1,10 @@
+<?php  
+    require_once(__DIR__."../../Model/Senha.php");
+    $senha = new Senha();
+    date_default_timezone_set('America/Sao_Paulo');
+    $data = date("Y-m-d H:i:s");
+    $troca = $senha->reCall($_POST['senha'], $data);
+    $response = array("success" => true, "troca" => $troca );
+    echo json_encode($response);
+
+?>
---
+++ app/Controller/resetarBanco.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once(__DIR__."../../Model/Conexao.php");
+    Conexao::resetarBanco();
+
+
+?>
