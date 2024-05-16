668236c8732330a76546427ebc87ade056281155
samugstu <samuelsouza0225@gmail.com>
2024-05-16 09:57:01 -0300

    Coisitas lindas e bunitas

--- area-admin/componentes/menu.php
+++ area-admin/componentes/menu.php
@@ -1,12 +1,11 @@
-<div class="row w-100 menu">
+<div class="row m-0 w-100 menu bg-success">
      <div class="col d-flex justify-content-center align-items-center text-light text-decoration-underline"><a href="controleSalas.php"><img src="./imgs/room.png" alt="" class="imga"></a></div>
      <div class="col d-flex justify-content-center align-items-center text-light text-decoration-underline"><a href="database.php"><img src="./imgs/database.png" alt="" class="imga"></a></div>
 </div>
 
 <style>
   .menu{
-    height: 12%;
-    background-color: green;
+    height: 15%;
   }
   .imga {
     height: 50px;
